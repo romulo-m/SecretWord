939b6c7ffda5503116ad726ca0c1964fb2da9359
Broad <romulo.mgaudencio@gmail.com>
2022-11-02 15:32:46 -0300

    renderização condicional e estados gerais

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// CSS
 import './App.css'
 
+// React
+import { useCallback, useEffect, useState } from 'react'
+
+// data
+import { wordsList } from '../data/words'
+
+// components
+import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+]
+
 function App() {
-  const [count, setCount] = useState(0)
+ const [gameStage, setGameStage] = useState(stages[0].name);
+ const [words] = useState(wordsList)
+
+ const [pickedWord, setPickedWord] = useState('')
+ const [pickedCategory, setPickedCategory] = useState('')
+ const [letters, setLetters] = useState([])
+
+ const [guessedLetters, setGuessedLetters] = useState([]);
+ const [wrongLetters, setWrongLetters] = useState([]);
+ const [guesses, setGuesses] = useState(3);
+ const [score, setScore] = useState(0)
+
+const pickeWordAndCategory = () => {
+  // pick a random category
+  const categories = Object.keys(words)
+  const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+  console.log(category);
+
+  //pick a random word
+  const word = words[category][Math.floor(Math.random() * words[category].length)]
+  console.log(word);
+
+  return { word, category }
+}
+
+// starts the secret word game
+ const startGame = () => {
+  // pick word and pick category
+ const { word, category } = pickeWordAndCategory();
+
+ // create an array of letters
+ let wordLetters = word.split('')
+
+ wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+ console.log(wordLetters)
+
+ // fill states
+ setPickedWord(word);
+ setPickedCategory(category);
+ setLetters(wordLetters);
+
+  setGameStage(stages[1].name);
+ }
+
+ // process the letter input
+ const verifyLetter = (letter) => {
+  console.log(letter)
+ }
+
+ // restarts the game
+ const retry = () => {
+  setGameStage(stages[0].name)
+ }
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+      {gameStage === 'game' && (
+        <Game 
+        verifyLetter={verifyLetter} 
+        pickedWord={pickedWord} 
+        pickedCategory={pickedCategory}
+        letters={letters}
+        guessedLetters={guessedLetters}
+        wrongLetters={wrongLetters}
+        guesses={guesses}
+        score={score}
+        />
+      )}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   )
 }
